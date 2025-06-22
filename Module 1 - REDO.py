1b79fd06ecd5aeca8cf94cfa309712087dbf676a
Bria007 <chebria.anderson@yahoo.com>
2025-06-21 20:49:11 -0500

    Add files via upload

---
+++ Module 1 - REDO.py
@@ -0,0 +1,29 @@
+Python 3.13.4 (v3.13.4:8a526ec7cbe, Jun  3 2025, 21:14:54) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> num1 = float(input("Enter the first number: "))
+Enter the first number: 1
+>>> num2 = float(input("Enter the second number: "))
+Enter the second number: 2
+>>> add = num1 + num2
+>>> subtract = num1 - num2
+>>> print("The sum is:", add)
+The sum is: 3.0
+>>> print("The difference is:", subtract)
+The difference is: -1.0
+>>> 
+>>> num1 = float(input("Enter the first number: "))
+Enter the first number: 1
+>>> num2 = float(input("Enter the second number: "))
+Enter the second number: 2
+>>> multiply = num1 * num2
+>>> if num2 != 0:
+...     divide = num1 / num2
+...     print("The product is:", multiply)
+...     print("The division result is:", divide)
+... else:
+...     print("The product is:", multiply)
+...     print("Cannot divide by zero.")
+... 
+...     
+The product is: 2.0
+The division result is: 0.5
